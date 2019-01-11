11db31ec3b4c3836e3e91b3f06d45f4be4220a29
mikf <mikf@dtu.dk>
2019-01-11 09:50:14 +0100

    Test pypi

--- setup.py
+++ setup.py
@@ -2,19 +2,30 @@
 """
 Setup file for PyWake
 """
-from py_wake import __version__
+import os
+from git_utils import write_vers
+from setuptools import setup, find_packages
 
-from setuptools import setup
+repo = os.path.dirname(__file__)
+version = write_vers(vers_file='py_wake/__init__.py', repo=repo, skip_chars=1)
+
+try:
+    from pypandoc import convert_file
+    read_md = lambda f: convert_file(f, 'rst', format='md')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
 
 setup(name='py_wake',
-      version=__version__,
+      version=version,
       description='PyWake a collection of wake models',
+      long_description=read_md('README.md'),
       url='https://gitlab.windenergy.dtu.dk/TOPFARM/PyWake',
       author='DTU Wind Energy',
       author_email='mmpe@dtu.dk',
       license='MIT',
-      packages=['py_wake'
-                ],
+      packages=find_packages(),
       install_requires=[
           'matplotlib',  # for plotting
           'numpy',  # for numerical calculations
