4785e136c22451cebe614ac28fac12b37b73fa0e
mikf <mikf@dtu.dk>
2020-01-10 14:08:55 +0100

    fixed a bug in the entry point for plugins

--- py_wake/__init__.py
+++ py_wake/__init__.py
@@ -3,7 +3,7 @@ import pkg_resources
 plugins = {
     entry_point.name: entry_point.load()
     for entry_point
-    in pkg_resources.iter_entry_points('topfarm.plugins')
+    in pkg_resources.iter_entry_points('py_wake.plugins')
 }
 
 # 'filled_by_setup.py'
