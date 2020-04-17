8e0ccf070ad971242a25c6ffc9ad21bbb22e5e3b
mmpe <mmpe@dtu.dk>
2020-04-17 15:10:08 +0200

    add publications to docs

--- docs/make_pdf.py
+++ docs/make_pdf.py
@@ -12,11 +12,13 @@ if __name__ == '__main__':
     import py_wake
     docs_path = Path(py_wake.__file__).parents[1] / 'docs'
     if os.path.isdir(docs_path / 'api'):
+        print("Switch to PDF mode")
         for ext in ['*.rst', 'notebooks/*.ipynb']:
             for f in docs_path.glob(ext):
                 svg2eps(f)
         (docs_path / 'api').rename("api_hide")
     else:
+        print("Switch to html mode")
         for ext in ['*.rst', 'notebooks/*.ipynb']:
             for f in docs_path.glob(ext):
                 svg2eps(f, lambda s: s.replace('.eps', '.svg'))
