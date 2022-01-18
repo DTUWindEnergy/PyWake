fix a exp overflow issue + test and disregard speedup(i) in flow_map(x,y)

py_wake/deficit_models/gaussian.py
@@ -313,8 +313,9 @@ class ZongGaussianDeficit(NiayifarGaussianDeficit):
         # initial size is here a combination of epsilon and the near-wake (needed modification, to ensure
         # the intial wake width is identical to the original formulation. Zong just used a fixed value)
         # non-dimensional wake expansion
-        sigmaD_ijlk = self.epsilon_ilk(ct_ilk)[:, na] + k_ilk[:, na, :, :] * \
-            np.log(1 + np.exp((dw_ijlk - xnw_ilk[:, na, :, :]) / D_src_il[:, na, :, na]))
+        # logaddexp(0,x) ~ log(1+exp(x)) without overflow
+        sigmaD_ijlk = (self.epsilon_ilk(ct_ilk)[:, na] + k_ilk[:, na, :, :] *
+                       np.logaddexp(0, (dw_ijlk - xnw_ilk[:, na, :, :]) / D_src_il[:, na, :, na]))
 
         return sigmaD_ijlk * D_src_il[:, na, :, na]
 

py_wake/site/xrsite.py
@@ -10,6 +10,7 @@ from py_wake.utils import weibull
 from py_wake.utils.ieawind37_utils import iea37_names
 from py_wake.utils.grid_interpolator import GridInterpolator, EqDistRegGrid2DInterpolator
 import urllib.request
+import warnings
 
 
 class XRSite(Site):
@@ -214,7 +215,10 @@ class XRSite(Site):
         WS, WD, TI, TI_std = [get(n, d) for n, d in [('WS', lw.ws), ('WD', lw.wd), ('TI', None), ('TI_std', None)]]
 
         if 'Speedup' in self.ds:
-            WS = self.interp(self.ds.Speedup, lw.coords) * WS
+            if 'i' in lw.dims and 'i' in self.ds.Speedup.dims and len(lw.i) != len(self.ds.i):
+                warnings.warn("Speedup ignored")
+            else:
+                WS = self.interp(self.ds.Speedup, lw.coords) * WS
 
         if self.shear:
             assert 'h' in lw and np.all(lw.h.data != None), "Height must be specified and not None"  # nopep8
@@ -226,7 +230,10 @@ class XRSite(Site):
             WS = self.shear(WS, lw.wd, h)
 
         if 'Turning' in self.ds:
-            WD = (self.interp(self.ds.Turning, lw.coords) + WD) % 360
+            if 'i' in lw.dims and 'i' in self.ds.Turning.dims and len(lw.i) != len(self.ds.i):
+                warnings.warn("Turning ignored")
+            else:
+                WD = (self.interp(self.ds.Turning, lw.coords) + WD) % 360
 
         lw.set_W(WS, WD, TI, ws_bins, self.use_WS_bins)
         lw.set_data_array(TI_std, 'TI_std', 'Standard deviation of turbulence intensity')

py_wake/tests/test_deficit_models/test_deficit_models.py
@@ -3,7 +3,7 @@ import pytest
 import matplotlib.pyplot as plt
 import numpy as np
 from py_wake.deficit_models.deficit_model import WakeDeficitModel, BlockageDeficitModel, DeficitModel
-from py_wake.deficit_models.fuga import FugaDeficit, Fuga
+from py_wake.deficit_models.fuga import FugaDeficit, Fuga, FugaYawDeficit
 from py_wake.deficit_models.gaussian import BastankhahGaussianDeficit, IEA37SimpleBastankhahGaussianDeficit,\
     ZongGaussianDeficit, NiayifarGaussianDeficit, BastankhahGaussian, IEA37SimpleBastankhahGaussian, ZongGaussian,\
     NiayifarGaussian, CarbajofuertesGaussianDeficit, TurboGaussianDeficit
@@ -110,6 +110,57 @@ def test_IEA37_ex16(deficitModel, aep_ref):
 
     npt.assert_almost_equal(aep_MW_l.sum(), aep_ref[0], 5)
     npt.assert_array_almost_equal(aep_MW_l, aep_ref[1], 5)
+
+
+@pytest.mark.parametrize('deficitModel', get_models(WakeDeficitModel))
+def test_huge_distance(deficitModel):
+    ref = {"NOJDeficit": 9.799728,
+           "FugaDeficit": 9.8,
+           "FugaYawDeficit": 9.8,
+           "BastankhahGaussianDeficit": 9.799146,
+           "CarbajofuertesGaussianDeficit": 9.798708,
+           "IEA37SimpleBastankhahGaussianDeficit": 9.799151,
+           "NiayifarGaussianDeficit": 9.799148,
+           "TurboGaussianDeficit": 9.793684,
+           "ZongGaussianDeficit": 9.799146,
+           "GCLDeficit": 9.728704,
+           "NoWakeDeficit": 9.8,
+           "NOJLocalDeficit": 9.797488,
+           "TurboNOJDeficit": 9.795231, }
+    site = IEA37Site(16)
+
+    windTurbines = IEA37_WindTurbines()
+    wfm = All2AllIterative(site, windTurbines, wake_deficitModel=deficitModel(), turbulenceModel=GCLTurbulence())
+    sim_res = wfm([0, 100000], [0, 0], wd=[0, 90, 180, 270])
+    # print(f'"{deficitModel.__name__}": {np.round(sim_res.WS_eff.sel(wt=1, ws=9.8, wd=270).item(),6)},')
+
+    npt.assert_array_almost_equal([9.8, 9.8, 9.8, ref[deficitModel.__name__]], sim_res.WS_eff.sel(wt=1).squeeze())
+
+
+@pytest.mark.parametrize('deficitModel', get_models(BlockageDeficitModel))
+def test_huge_distance_blockage(deficitModel):
+    if deficitModel is None:
+        return
+    ref = {"FugaDeficit": 9.8,
+           "FugaYawDeficit": 9.8,
+           "HybridInduction": 9.799999,
+           "SelfSimilarityDeficit2020": 9.799999,
+           "VortexDipole": 9.799999,
+           "RankineHalfBody": 9.799999,
+           "Rathmann": 9.799999,
+           "RathmannScaled": 9.799999,
+           "SelfSimilarityDeficit": 9.799999,
+           "VortexCylinder": 9.799999, }
+    site = IEA37Site(16)
+
+    windTurbines = IEA37_WindTurbines()
+    wfm = All2AllIterative(site, windTurbines, wake_deficitModel=NoWakeDeficit(),
+                           blockage_deficitModel=deficitModel(),
+                           turbulenceModel=GCLTurbulence())
+    sim_res = wfm([0, 100000], [0, 0], wd=[0, 90, 180, 270])
+    # print(f'"{deficitModel.__name__}": {np.round(sim_res.WS_eff.sel(wt=0, ws=9.8, wd=270).item(),6)},')
+
+    npt.assert_array_almost_equal([9.8, 9.8, 9.8, ref[deficitModel.__name__]], sim_res.WS_eff.sel(wt=1).squeeze())
 
 
 @pytest.mark.parametrize(

py_wake/wind_turbines/_wind_turbines.py
@@ -239,12 +239,12 @@ Use WindTurbines(names, diameters, hub_heights, power_ct_funcs) instead""", Depr
     def plot(self, x, y, type=None, wd=None, yaw=0, tilt=0, normalize_with=1, ax=None):
         return self.plot_xy(x, y, type, wd, yaw, tilt, normalize_with, ax)
 
-    def plot_power_ct(self, ax=None, **kwargs):
+    def plot_power_ct(self, ax=None, **wt_kwargs):
         import matplotlib.pyplot as plt
         ws = np.linspace(0, 25, 1000)
         if ax is None:
             ax = plt.gca()
-        power, ct = self.power_ct(ws, **kwargs)
+        power, ct = self.power_ct(ws, **wt_kwargs)
         ax.plot(ws, power)
         ax.grid()
         ax2 = ax.twinx()