3ac4637877a10c30a436dc80c68a250534def1c1
Mads M. Pedersen <mmpe@dtu.dk>
2022-03-02 07:46:47 +0000

    implement support for fd, cs and autograd in TensorflowSurrogate (used for...

--- py_wake/tests/test_load_surrogates/test_iea34_surrogates.py
+++ py_wake/tests/test_load_surrogates/test_iea34_surrogates.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
 from py_wake.examples.data.iea34_130rwt._iea34_130rwt import IEA34_130_1WT_Surrogate, IEA34_130_2WT_Surrogate
 from py_wake.tests import npt
 from py_wake.deficit_models.noj import NOJ
@@ -10,9 +10,21 @@ from pathlib import Path
 from py_wake.wind_turbines.wind_turbine_functions import FunctionSurrogates
 from py_wake.utils.tensorflow_surrogate_utils import TensorflowSurrogate
 from py_wake.examples.data import example_data_path
+from py_wake.utils.gradients import plot_gradients, fd, autograd, cs
+import pytest
 
 
-def test_one_turbine_case0():
+@pytest.fixture(scope='module')
+def iea34_130_1WT_Surrogate():
+    return IEA34_130_1WT_Surrogate()
+
+
+@pytest.fixture(scope='module')
+def iea34_130_2WT_Surrogate():
+    return IEA34_130_2WT_Surrogate()
+
+
+def test_one_turbine_case0(iea34_130_1WT_Surrogate):
     ws, ti, shear = 9.2984459862, 0.0597870198, 0.2
 
     if 0:
@@ -37,7 +49,7 @@ def test_one_turbine_case0():
     thrust_ref = 5.408776e+02
     ref_dels = [1822, 5795, 4385, 2468, 1183]
 
-    wt = IEA34_130_1WT_Surrogate()
+    wt = iea34_130_1WT_Surrogate
     assert wt.loadFunction.output_keys[1] == 'del_blade_edge'
     assert wt.loadFunction.wohler_exponents == [10, 10, 4, 4, 7]
     site = UniformSite(p_wd=[1], ti=ti, ws=ws)
@@ -59,7 +71,7 @@ def test_one_turbine_case0():
     npt.assert_array_almost_equal(loads.LDEL.squeeze(), (loads.DEL.squeeze()**m * f)**(1 / m))
 
 
-def test_two_turbine_case0():
+def test_two_turbine_case0(iea34_130_2WT_Surrogate):
     if 0:
         i = 0
         f = r'C:\mmpe\programming\python\Topfarm\iea-3_4-130-rwt\turbine_model\res/'
@@ -89,7 +101,7 @@ def test_two_turbine_case0():
     power_ref = 3.399746e+06
     ref_dels = [4546, 5931, 11902, 7599, 2407]
 
-    wt = IEA34_130_2WT_Surrogate()
+    wt = iea34_130_2WT_Surrogate
     site = UniformSite(p_wd=[1], ti=ti, ws=ws)
     sim_res = NOJ(site, wt, turbulenceModel=STF2017TurbulenceModel())([0, 0], [0, dist * 130], wd=wdir, Alpha=shear)
 
@@ -111,7 +123,7 @@ def test_two_turbine_case0():
     npt.assert_array_almost_equal(loads.LDEL.sel(wt=0).squeeze(), (loads.DEL.sel(wt=0).squeeze()**m * f)**(1 / m))
 
 
-def test_two_turbine_case0_time_series():
+def test_two_turbine_case0_time_series(iea34_130_2WT_Surrogate):
     # same as test_two_turbine_case0
     ws, ti, shear, wdir, dist = [10.9785338191, 0.2623204277, 0.4092031776, -38.4114616871 % 360, 5.123719529]
 
@@ -121,7 +133,7 @@ def test_two_turbine_case0_time_series():
     power_ref = 3.399746e+06
     ref_dels = [4546, 5931, 11902, 7599, 2407]
 
-    wt = IEA34_130_2WT_Surrogate()
+    wt = iea34_130_2WT_Surrogate
     site = UniformSite(p_wd=[1], ti=ti, ws=ws)
     wfm = NOJ(site, wt, turbulenceModel=STF2017TurbulenceModel())
     sim_res = wfm([0, 0], [0, dist * 130], wd=wdir, time=True, Alpha=shear)
@@ -157,3 +169,69 @@ def test_functionSurrogate():
         'MomentMx Mbdy:blade1 nodenr:   1 coo: blade1  blade root moment blade1',
         'MomentMy Mbdy:blade1 nodenr:   1 coo: blade1  blade root moment blade1']
     npt.assert_array_almost_equal(loadFunction(np.array([10, 11])), [[2077.9673, 2116.636], [5710.0894, 5653.4956]], 3)
+
+
+def test_ws_gradients(iea34_130_1WT_Surrogate):
+    wt = iea34_130_1WT_Surrogate
+    ws = np.linspace(3, 20, 1000)
+    x = 7.
+    plt.plot(ws, wt.power(ws, TI_eff=0.1))
+
+    method_lst = [lambda *args, **kwargs: fd(*args, step=.001, **kwargs), cs, autograd]
+    dpdx_lst = [method(wt.power)(x, TI_eff=0.1) for method in method_lst]
+    npt.assert_allclose(dpdx_lst[0], dpdx_lst[1], rtol=.0005)
+    npt.assert_array_almost_equal(dpdx_lst[1], dpdx_lst[2], 10)
+    if 0:
+        y = wt.power(x, TI_eff=0.1)
+        for method, dpdx in zip(method_lst, dpdx_lst):
+            plot_gradients(y, dpdx, x, label=method.__name__)
+        plt.show()
+
+
+def test_ti_gradients(iea34_130_1WT_Surrogate):
+    wt = iea34_130_1WT_Surrogate
+    ti_low, ti_high = wt.powerCtFunction.function_surrogate_lst[0].input_space['ti']
+    ws_lst = np.linspace(3, 20, 1000)
+    ti_lst = np.linspace(ti_low, ti_high, 100)
+
+    def power(ws, ti):
+        return wt.power(ws, TI_eff=ti)
+    if 0:
+        for ti in np.linspace(ti_low, ti_high, 5):
+            plt.plot(ws_lst, wt.power(ws_lst, TI_eff=ti), label=ti)
+        plt.show()
+    ws = 10
+    x = .1
+
+    method_lst = [lambda *args, **kwargs: fd(*args, step=.001, **kwargs), cs, autograd]
+    dpdx_lst = [method(power, argnum=1)(ws, x) for method in method_lst]
+    npt.assert_allclose(dpdx_lst[0], dpdx_lst[1], rtol=.005)
+    npt.assert_array_almost_equal(dpdx_lst[1], dpdx_lst[2], 10)
+    if 0:
+        plt.plot(ti_lst, wt.power(ti_lst * 0 + ws, TI_eff=ti))
+        y = power(ws, x)
+        for method, dpdx in zip(method_lst, dpdx_lst):
+            plot_gradients(y, dpdx, x, label=method.__name__, step=.1)
+        plt.show()
+
+
+def test_ws_and_ti_gradients(iea34_130_1WT_Surrogate):
+    wt = iea34_130_1WT_Surrogate
+    ws_lst = np.linspace(9, 11, 100)
+
+    def power(ws):
+        return wt.power(ws, TI_eff=ws / 50)
+
+    ws = 10
+
+    method_lst = [lambda *args, **kwargs: fd(*args, step=.001, **kwargs), cs, autograd]
+    dpdx_lst = [method(power)(ws) for method in method_lst]
+    npt.assert_allclose(dpdx_lst[0], dpdx_lst[1], rtol=.005)
+    npt.assert_array_almost_equal(dpdx_lst[1], dpdx_lst[2], 10)
+    if 0:
+        plt.plot(ws_lst, wt.power(ws_lst, TI_eff=0.001))
+        plt.plot(ws_lst, power(ws_lst))
+        y = power(ws)
+        for method, dpdx in zip(method_lst, dpdx_lst):
+            plot_gradients(y, dpdx, ws, label=method.__name__, step=.1)
+        plt.show()
