fix and test gradients of SimpleYawModel, DensityScale, PowerCtNDTabular,...

py_wake/deficit_models/fuga.py
@@ -199,21 +199,11 @@ class LUTInterpolator(object):
         xp, yp, zp = xyz
         xp = np.maximum(np.minimum(xp, self.x[-1]), self.x[0])
         yp = np.maximum(np.minimum(yp, self.y[-1]), self.y[0])
-        # zp = np.maximum(np.minimum(zp, self.z[-1]), self.z[0])
-        from autograd.numpy.numpy_boxes import ArrayBox
 
-        def i0f(_i):
-            if isinstance(_i, ArrayBox):
-                _i0 = _i._value.astype(int)
-            else:
-                _i0 = np.real(_i).astype(int)
-            _if = _i - _i0
-            return _i0, _if
+        xif, xi0 = gradients.modf((xp - self.x0) / self.dx)
+        yif, yi0 = gradients.modf((yp - self.y0) / self.dy)
 
-        xi0, xif = i0f((xp - self.x0) / self.dx)
-        yi0, yif = i0f((yp - self.y0) / self.dy)
-
-        zi0, zif = i0f(gradients.interp(zp, self.z, np.arange(self.nz)))
+        zif, zi0 = gradients.modf(gradients.interp(zp, self.z, np.arange(self.nz)))
 
         nx, ny = self.nx, self.ny
         idx = zi0 * nx * ny + yi0 * nx + xi0

py_wake/site/distance.py
@@ -2,12 +2,13 @@ import numpy as np
 from numpy import newaxis as na
 import matplotlib
 from py_wake.utils.functions import mean_deg
+from py_wake.utils import gradients
 
 
 class StraightDistance():
 
     def _cos_sin(self, wd):
-        theta = np.deg2rad(90 - wd)
+        theta = gradients.deg2rad(90 - wd)
         cos = np.cos(theta)
         sin = np.sin(theta)
         return cos, sin
@@ -61,6 +62,7 @@ class StraightDistance():
 
     def __call__(self, WD_il, wd_l=None, src_idx=slice(None), dst_idx=slice(None)):
         assert hasattr(self, 'dx_ij'), "method setup must be called first"
+        WD_il = np.asarray(WD_il)
         if len(np.shape(WD_il)) == 1:
             cos_l, sin_l = self._cos_sin(WD_il)
             dx_ij, dy_ij = self.dx_ii[src_idx, dst_idx], self.dy_ii[src_idx, dst_idx]
@@ -132,7 +134,7 @@ class TerrainFollowingDistance(StraightDistance):
         else:
             d_ij = s.reshape(self.dx_ij.shape)
         self.d_ij = d_ij
-        self.theta_ij = np.arctan2(self.dst_y_j - self.src_y_i[:, na], self.dst_x_j - self.src_x_i[:, na])
+        self.theta_ij = gradients.arctan2(self.dst_y_j - self.src_y_i[:, na], self.dst_x_j - self.src_x_i[:, na])
 
     def __call__(self, WD_il, wd_l=None, src_idx=slice(None), dst_idx=slice(None)):
         # instead of projecting the distances onto first x,y and then onto down wind direction

py_wake/tests/test_windturbines/test_power_ct_curves.py
@@ -9,6 +9,15 @@ from py_wake.wind_turbines.power_ct_functions import CubePowerSimpleCt, PowerCtN
     PowerCtTabular, PowerCtFunction, PowerCtFunctionList, PowerCtXr
 from py_wake.wind_turbines.wind_turbine_functions import WindTurbineFunction
 from py_wake.utils.model_utils import fix_shape
+from py_wake.utils.gradients import cs, autograd, fd
+
+
+def ExamplePowerCtTabular(method='linear', unit='w', p_scale=1):
+    u_p, p = np.asarray(hornsrev1.power_curve).T.copy()
+    p *= p_scale
+    u_ct, ct = hornsrev1.ct_curve.T
+    npt.assert_array_equal(u_p, u_ct)
+    return PowerCtTabular(ws=u_p, power=p, power_unit=unit, ct=ct, ws_cutin=4, ws_cutout=25, method=method)
 
 
 @pytest.mark.parametrize('method,unit,p_scale,p_ref,ct_ref', [
@@ -25,11 +34,7 @@ from py_wake.utils.model_utils import fix_shape
      [0.0, 0.0, 364247.229, 1167112.52, 1922219.683, 1998242.424, 2000000.0, 2000000.0, 2000000.0, 0.0],
      [0.0, 0.0, 0.804, 0.803, 0.57, 0.224, 0.129, 0.082, 0.058, 0.0])])
 def test_TabularPowerCtCurve(method, unit, p_scale, p_ref, ct_ref):
-    u_p, p = np.asarray(hornsrev1.power_curve).T.copy()
-    p *= p_scale
-    u_ct, ct = hornsrev1.ct_curve.T
-    npt.assert_array_equal(u_p, u_ct)
-    curve = PowerCtTabular(ws=u_p, power=p, power_unit=unit, ct=ct, ws_cutin=4, ws_cutout=25, method=method)
+    curve = ExamplePowerCtTabular(method, unit, p_scale)
     npt.assert_array_equal(curve.optional_inputs, ['Air_density', 'tilt', 'yaw'])
     npt.assert_array_equal(curve.required_inputs, [])
 
@@ -52,6 +57,11 @@ def test_TabularPowerCtCurve(method, unit, p_scale, p_ref, ct_ref):
     p, ct = curve(u, run_only=0), curve(u, run_only=1)
     npt.assert_array_almost_equal(p[s], p_ref, 3)
     npt.assert_array_almost_equal(ct[s], ct_ref, 3)
+
+    for run_only in [0, 1]:
+        dpctdu_lst = [grad(curve)([10, 12], run_only=run_only) for grad in [fd, cs, autograd]]
+        npt.assert_allclose(dpctdu_lst[0], dpctdu_lst[1], rtol=1e-4)
+        npt.assert_allclose(dpctdu_lst[1], dpctdu_lst[2])
 
 
 def test_MultiPowerCtCurve():
@@ -83,6 +93,11 @@ def test_MultiPowerCtCurve():
     for m in [mode_16, mode_16_360, mode_16_360_23]:
         p, ct = curve(u, mode=m)
         npt.assert_array_almost_equal(p, ref_p)
+
+    for run_only in [0, 1]:
+        dpctdu_lst = [grad(curve)([10, 12], mode=0, run_only=run_only) for grad in [fd, cs, autograd]]
+        npt.assert_allclose(dpctdu_lst[0], dpctdu_lst[1], rtol=1e-4)
+        npt.assert_allclose(dpctdu_lst[1], dpctdu_lst[2])
 
 
 def test_MultiMultiPowerCtCurve_subset():
@@ -158,6 +173,14 @@ def test_2d_tabular():
         p, ct = curve(u, boost=b)
         npt.assert_array_almost_equal(p, ref_p)
 
+    for run_only in [0, 1]:
+        for argnum in [0, 1]:
+            def t(u, boost):
+                return curve(u, boost=boost, run_only=run_only)
+            dpctdu_lst = [grad(t, argnum=argnum)([10, 12], boost=[.5, .5]) for grad in [fd, cs, autograd]]
+            npt.assert_allclose(dpctdu_lst[0], dpctdu_lst[1], rtol=1e-4)
+            npt.assert_allclose(dpctdu_lst[1], dpctdu_lst[2])
+
 
 def test_2d_tabular_default_value():
     u_p, p_c = np.asarray(hornsrev1.power_curve).T.copy()
@@ -194,6 +217,14 @@ def test_FunctionalPowerCtCurve():
                                                   5000000.0, 5000000.0, 5000000.0, 0.0]) * 1.3 / 1.225, 3)
     npt.assert_array_almost_equal(ct[s], np.array([0.03, 0.889, 0.889, 0.889, 0.824, 0.489,
                                                    0.245, 0.092, 0.031, 0.03]) * 1.3 / 1.225, 3)
+
+    for run_only in [0, 1]:
+        for argnum in [0, 1]:
+            def t(u, Air_density):
+                return curve(u, Air_density=Air_density, run_only=run_only)
+            dpctdu_lst = [grad(t, argnum=argnum)([10, 13], Air_density=[1.225, 1.5]) for grad in [fd, cs, autograd]]
+            npt.assert_allclose(dpctdu_lst[0], dpctdu_lst[1], rtol=1e-4)
+            npt.assert_allclose(dpctdu_lst[1], dpctdu_lst[2])
 
 
 def get_continuous_curve(key, optional):
@@ -252,6 +283,13 @@ def test_PowerCtXr():
         coords={'boost': [0, 10], 'ws': u_p})
     curve = PowerCtXr(ds, 'w')
     npt.assert_array_almost_equal(curve(u_p, boost=0), curve(u_p, boost=10) / 1.1)
+    for run_only in [0, 1]:
+        for argnum in [0, 1]:
+            def t(u, boost):
+                return curve(u, boost=boost, run_only=run_only)
+            dpctdu_lst = [grad(t, argnum=argnum)([10, 12], boost=[.5, .5]) for grad in [fd, cs, autograd]]
+            npt.assert_allclose(dpctdu_lst[0], dpctdu_lst[1], rtol=1e-4)
+            npt.assert_allclose(dpctdu_lst[1], dpctdu_lst[2])
 
 
 def test_missing_input_PowerCtFunctionList():
@@ -285,6 +323,14 @@ def test_density_scale():
         npt.assert_array_almost_equal(p, np.interp(u, u_p, p_c) * 1.3 / rho_ref)
         npt.assert_array_almost_equal(ct, np.interp(u, u_p, ct_c) * 1.3 / rho_ref)
 
+    for run_only in [0, 1]:
+        for argnum in [0, 1]:
+            def t(u, Air_density):
+                return curve(u, Air_density=Air_density, run_only=run_only)
+            dpctdu_lst = [grad(t, argnum=argnum)([10, 13], Air_density=[1.225, 1.5]) for grad in [fd, cs, autograd]]
+            npt.assert_allclose(dpctdu_lst[0], dpctdu_lst[1], rtol=1e-4)
+            npt.assert_allclose(dpctdu_lst[1], dpctdu_lst[2])
+
 
 def test_SimpleYawModel():
     u_p, p_c = np.asarray(hornsrev1.power_curve).T.copy()
@@ -298,6 +344,14 @@ def test_SimpleYawModel():
     npt.assert_array_almost_equal(p, np.interp(u * co, u_p, p_c))
     npt.assert_array_almost_equal(ct, np.interp(u * co, u_p, ct_c) * co**2)
 
+    for run_only in [0, 1]:
+        for argnum in [0, 1]:
+            def t(u, yaw):
+                return curve(u, yaw=yaw, run_only=run_only)
+            dpctdu_lst = [grad(t, argnum=argnum)([10, 13], yaw=[10, 20]) for grad in [fd, cs, autograd]]
+            npt.assert_allclose(dpctdu_lst[0], dpctdu_lst[1], rtol=1e-4)
+            npt.assert_allclose(dpctdu_lst[1], dpctdu_lst[2])
+
 
 def test_DensityScaleAndSimpleYawModel():
     u_p, p_c = np.asarray(hornsrev1.power_curve).T.copy()
@@ -310,6 +364,15 @@ def test_DensityScaleAndSimpleYawModel():
     p, ct = curve(u, yaw=yaw, Air_density=1.3)
     npt.assert_array_almost_equal(p, np.interp(u * co, u_p, p_c) * 1.3 / 1.225)
     npt.assert_array_almost_equal(ct, np.interp(u * co, u_p, ct_c) * co**2 * 1.3 / 1.225)
+
+    for run_only in [0, 1]:
+        for argnum in [0, 1, 2]:
+            def t(u, Air_density, yaw):
+                return curve(u, Air_density=Air_density, yaw=yaw, run_only=run_only)
+            dpctdu_lst = [grad(t, argnum=argnum)([10, 13], Air_density=[1.225, 1.5], yaw=[10, 20])
+                          for grad in [fd, cs, autograd]]
+            npt.assert_allclose(dpctdu_lst[0], dpctdu_lst[1], rtol=1e-4)
+            npt.assert_allclose(dpctdu_lst[1], dpctdu_lst[2])
 
 
 if __name__ == '__main__':

py_wake/utils/functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from py_wake.utils import gradients
 
 
 def mean_deg(dir, axis=0):
@@ -16,7 +17,7 @@ def mean_deg(dir, axis=0):
     mean_deg : float
         Mean angle
     """
-    return np.rad2deg(mean_rad(np.deg2rad(dir), axis))
+    return gradients.rad2deg(mean_rad(gradients.deg2rad(dir), axis))
 
 
 def mean_rad(dir, axis=0):
@@ -34,4 +35,4 @@ def mean_rad(dir, axis=0):
     mean_rad : float
         Mean angle
     """
-    return np.arctan2(np.nanmean(np.sin(dir[:]), axis), np.nanmean(np.cos(dir[:]), axis))
+    return gradients.arctan2(np.nanmean(np.sin(dir[:]), axis), np.nanmean(np.cos(dir[:]), axis))

py_wake/utils/grid_interpolator.py
@@ -1,5 +1,7 @@
 import numpy as np
 from numpy import newaxis as na
+from py_wake.utils import gradients
+from autograd.numpy.numpy_boxes import ArrayBox
 
 
 class GridInterpolator(object):
@@ -57,7 +59,10 @@ class GridInterpolator(object):
         bounds = bounds or self.bounds
         assert method in ['linear', 'nearest'], 'method must be "linear" or "nearest"'
         assert bounds in ['check', 'limit', 'ignore'], 'bounds must be "check", "limit" or "ignore"'
-        xp = np.asarray(xp)
+        xp = np.atleast_2d(xp)
+        xp_shape = xp.shape
+        assert xp_shape[-1] == len(self.x), xp_shape
+        xp = np.reshape(xp, (-1, xp_shape[-1]))
         xpi = (xp - self.x0) / self.dx
         if len(self.irregular_axes):
             irreg_i = np.array([np.searchsorted(self.x[i], xp[:, i], side='right') - 1
@@ -77,8 +82,9 @@ class GridInterpolator(object):
                              (point, dimension, np.atleast_2d(xp)[point, dimension], self.x[dimension][0], self.x[dimension][-1]))
         if bounds == 'limit':
             xpi = np.minimum(np.maximum(xpi, 0), self.n - 1)
-        xpi0 = xpi.astype(int)
-        xpif = xpi - xpi0
+
+        xpif, xpi0 = gradients.modf(xpi)
+
         if method == 'nearest':
             xpif = np.round(xpif)
 
@@ -87,7 +93,7 @@ class GridInterpolator(object):
         indexes = np.minimum(indexes, (self.n - 1)[:, na, na])
         v = np.moveaxis(self.V[tuple(indexes)], [0, 1], [-2, -1])
         if deg:
-            v = (v + 180) % 360 - 180
+            v = (v + 180) % 360 - 180  # -180..180 > 0-360
 
         xpif1 = 1 - xpif
         # w = np.product([xpif10_.T[ui] for xpif10_, ui in zip(np.array([xpif1, xpif]).T, self.ui.T)], 0).T # slower
@@ -98,14 +104,14 @@ class GridInterpolator(object):
                 return weights
             else:
 
-                return [mul_weight(weights * xpif1[:, i], i + 1), mul_weight(weights * xpif[:, i], i + 1)]
+                return np.array([mul_weight(weights * xpif1[:, i], i + 1), mul_weight(weights * xpif[:, i], i + 1)])
 
         w = np.reshape(mul_weight(1, 0), (-1, xpif.shape[0]))
 
         res = np.moveaxis((w * v).sum(-2), -1, 0)
         if deg:
-            res = res % 360
-        return res
+            res = gradients.mod(res, 360)
+        return np.reshape(res, xp_shape[:-1] + self.V.shape[len(self.x):])
 
 
 class EqDistRegGrid2DInterpolator():
@@ -123,13 +129,10 @@ class EqDistRegGrid2DInterpolator():
 
     def __call__(self, x, y, mode='valid'):
         xp, yp = x, y
-        xi = (xp - self.x0) / self.dx
-        xif, xi0 = np.modf(xi)
-        xi0 = xi0.astype(int)
 
-        yi = (yp - self.y0) / self.dy
-        yif, yi0 = np.modf(yi)
-        yi0 = yi0.astype(int)
+        xif, xi0 = gradients.modf((xp - self.x0) / self.dx)
+        yif, yi0 = gradients.modf((yp - self.y0) / self.dy)
+
         if mode == 'extrapolate':
             xif[xi0 < self.xi_valid_min] = 0
             xif[xi0 > self.xi_valid_max - 2] = 1

py_wake/wind_turbines/power_ct_functions.py
@@ -8,6 +8,7 @@ from py_wake.utils.check_input import check_input
 from py_wake.utils.model_utils import check_model, fix_shape
 from py_wake.utils import gradients
 from py_wake.utils.gradients import PchipInterpolator, UnivariateSpline, set_vjp
+from py_wake.utils.grid_interpolator import GridInterpolator
 
 
 """
@@ -105,9 +106,9 @@ class SimpleYawModel(AdditionalModel):
         if yaw is not None or tilt is not None:
             co = 1
             if yaw is not None:
-                co *= np.cos(np.deg2rad(fix_shape(yaw, ws, True)))
+                co *= np.cos(gradients.deg2rad(fix_shape(yaw, ws, True)))
             if tilt is not None:
-                co *= np.cos(np.deg2rad(fix_shape(tilt, ws, True)))
+                co *= np.cos(gradients.deg2rad(fix_shape(tilt, ws, True)))
             power_ct_arr = f(ws * co, **kwargs)  # calculate for reduced ws (ws projection on rotor)
             if kwargs['run_only'] == 1:  # ct
                 # multiply ct by cos(yaw)**2 to compensate for reduced thrust
@@ -128,7 +129,8 @@ class DensityScale(AdditionalModel):
     def __call__(self, f, ws, Air_density=None, **kwargs):
         power_ct_arr = np.asarray(f(ws, **kwargs))
         if Air_density is not None:
-            power_ct_arr *= fix_shape(Air_density, ws, True) / self.air_density_ref
+            # cannot used *= if Air_density is complex
+            power_ct_arr = power_ct_arr * fix_shape(Air_density, ws, True) / self.air_density_ref
         return power_ct_arr
 
 
@@ -332,20 +334,21 @@ class PowerCtNDTabular(PowerCtFunction):
             list of additional models.
         """
         self.default_value_dict = default_value_dict
-        self.interp = [RegularGridInterpolator(value_lst, power_arr),
-                       RegularGridInterpolator(value_lst, ct_arr)]
+        self.interp = [GridInterpolator(value_lst, power_arr),
+                       GridInterpolator(value_lst, ct_arr)]
+
         PowerCtFunction.__init__(self, input_keys, self._power_ct, power_unit,
                                  default_value_dict.keys(), additional_models)
 
     def _power_ct(self, ws, run_only, **kwargs):
         kwargs = {**self.default_value_dict, 'ws': ws, **{k: v for k, v in kwargs.items() if v is not None}}
 
-        args = np.moveaxis([fix_shape(kwargs[k], ws)
-                            for k in self.input_keys], 0, -1)
+        args = np.moveaxis(np.array([fix_shape(kwargs[k], ws)
+                                     for k in self.input_keys]), 0, -1)
         try:
             return self.interp[run_only](args)
         except ValueError:
-            check_input(self.interp[run_only].grid, args.T, self.input_keys)
+            check_input(self.interp[run_only].x, args.T, self.input_keys)
 
 
 class PowerCtXr(PowerCtNDTabular):