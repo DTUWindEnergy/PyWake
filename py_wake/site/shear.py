fix gradients of shear, GCLTurbulence and partial overlapping NOJDeficit

py_wake/site/shear.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy import newaxis as na
 from abc import abstractmethod, ABC
+from py_wake.utils.xarray_utils import DataArrayILK
 
 
 class Shear(ABC):
@@ -34,7 +35,7 @@ class PowerShear(Shear):
         alpha = self.alpha.interp_all(WD, method=self.interp_method)
         if alpha.shape == ():
             alpha = alpha.data
-        return (h / self.h_ref) ** alpha * WS
+        return DataArrayILK((h / self.h_ref) ** alpha) * WS
 
 
 class LogShear(Shear):
@@ -48,7 +49,7 @@ class LogShear(Shear):
         z0 = self.z0.interp_all(WD, method=self.interp_method)
         if z0.shape == ():
             z0 = z0.data
-        return np.log(h / z0) / np.log(self.h_ref / z0) * WS
+        return DataArrayILK(h / z0).log() / DataArrayILK(self.h_ref / z0).log() * WS
 
 
 # ======================================================================================================================

py_wake/tests/check_speed.py
@@ -12,6 +12,7 @@ from py_wake.examples.data.hornsrev1 import wt_x, wt_y, HornsrevV80, Hornsrev1Si
 from py_wake.tests import npt
 from py_wake.tests.test_files import tfp
 import sys
+from py_wake.utils.profiling import timeit
 
 path = tfp + 'fuga/2MW/Z0=0.03000000Zi=00401Zeta0=0.00E+00/'
 
@@ -21,39 +22,6 @@ def Fuga(site, wt):
 
 
 test_lst = [(NOJ, 1.2), (IEA37SimpleBastankhahGaussian, 1.5), (Fuga, 1)]
-
-
-def timeit(func, min_time=0, min_runs=1, verbose=False, line_profile=False, profile_funcs=[]):
-    @functools.wraps(func)
-    def newfunc(*args, **kwargs):
-        if line_profile and getattr(sys, 'gettrace')() is None:
-            from line_profiler import LineProfiler
-            lp = LineProfiler()
-            lp.timer_unit = 1e-6
-            for f in profile_funcs:
-                lp.add_function(f)
-            lp_wrapper = lp(func)
-            t = time.time()
-            res = lp_wrapper(*args, **kwargs)
-            t = time.time() - t
-            if verbose:
-                lp.print_stats()
-            return res, [t]
-        else:
-            t_lst = []
-            for i in range(100000):
-                startTime = time.time()
-                res = func(*args, **kwargs)
-                t_lst.append(time.time() - startTime)
-                if sum(t_lst) > min_time and len(t_lst) >= min_runs:
-                    if hasattr(func, '__name__'):
-                        fn = func.__name__
-                    else:
-                        fn = "Function"
-                    if verbose:
-                        print('%s: %f +/-%f (%d runs)' % (fn, np.mean(t_lst), np.std(t_lst), i + 1))
-                    return res, t_lst
-    return newfunc
 
 
 def check_speed_Hornsrev(WFModel, max_min):

py_wake/tests/test_wind_farm_models/test_memory_usage.py
@@ -10,19 +10,12 @@ import memory_profiler
 from py_wake.tests import npt
 import pytest
 from py_wake.deficit_models.selfsimilarity import SelfSimilarityDeficit
-
-
-def get_memory_usage():
-    pid = os.getpid()
-    python_process = psutil.Process(pid)
-    return python_process.memory_info()[0] / 1024**2
+from py_wake.utils.profiling import check_memory_usage, get_memory_usage
 
 
 def test_memory_usage():
     if os.name == 'posix':
         pytest.skip('Memory usage seems not to work on linux')
-    gc.collect()
-    initial_mem_usage = get_memory_usage()
     wt = V80()
     site = Hornsrev1Site()
     x, y = site.initial_position.T
@@ -34,12 +27,7 @@ def test_memory_usage():
                           blockage_deficitModel=SelfSimilarityDeficit()), 10, 288 - tol, 361 + tol)]:
         lst = []
         for _ in range(1):
-            initial_mem_usage = get_memory_usage()
-            gc.collect()
-            mem_usage, _ = memory_profiler.memory_usage(
-                (wfm, (x, y), {'wd': np.arange(0, 360, wd_step)}), interval=.01, max_usage=True, retval=True)
-
-            mem_usage -= initial_mem_usage
+            mem_usage, _ = check_memory_usage(wfm)(x, y, wd=np.arange(0, 360, wd_step))
             lst.append(mem_usage)
 
 #        print(np.min(lst), np.max(lst), np.mean(lst), np.std(lst))