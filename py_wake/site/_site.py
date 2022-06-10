fix gradients of TerrainFollowingDistance + gridded site with multiple ws

py_wake/site/_site.py
@@ -4,7 +4,7 @@ from py_wake.site.shear import PowerShear
 import py_wake.utils.xarray_utils  # register ilk function @UnusedImport
 import xarray as xr
 from abc import ABC, abstractmethod
-from py_wake.utils.xarray_utils import da2py
+from py_wake.utils.xarray_utils import da2py, DataArrayILK
 
 """
 suffixs:
@@ -74,18 +74,19 @@ class LocalWind(xr.Dataset):
         lattr = {'Description': 'Lower bound of wind speed bins [m/s]'}
         uattr = {'Description': 'Upper bound of wind speed bins [m/s]'}
         if not hasattr(ws_bins, '__len__') or len(ws_bins) != len(WS) + 1:
-            if len(WS.shape) and WS.shape[-1] > 1:
-                d = np.diff(WS) / 2
+            WS_ilk = WS.ilk()
+            if WS_ilk.shape[-1] > 1:
+                d = np.diff(WS_ilk) / 2
                 ws_bins = np.maximum(np.concatenate(
-                    [WS[..., :1] - d[..., :1], WS[..., :-1] + d, WS[..., -1:] + d[..., -1:]], -1), 0)
+                    [WS_ilk[..., :1] - d[..., :1], WS_ilk[..., :-1] + d, WS_ilk[..., -1:] + d[..., -1:]], -1), 0)
             else:
                 # WS is single value
                 if ws_bins is None:
                     ws_bins = 1
-                ws_bins = WS.data + np.array([-ws_bins / 2, ws_bins / 2])
+                ws_bins = WS_ilk + np.array([-ws_bins / 2, ws_bins / 2])
 
-            self['ws_lower'] = xr.DataArray(ws_bins[..., :-1], dims=WS.dims, attrs=lattr)
-            self['ws_upper'] = xr.DataArray(ws_bins[..., 1:], dims=WS.dims, attrs=uattr)
+            self['ws_lower'] = DataArrayILK(ws_bins[..., :-1], attrs=lattr).squeeze()
+            self['ws_upper'] = DataArrayILK(ws_bins[..., 1:], attrs=uattr).squeeze()
         else:
             self['ws_lower'] = xr.DataArray(ws_bins[:-1], dims=['ws'], attrs=lattr)
             self['ws_upper'] = xr.DataArray(ws_bins[1:], dims=['ws'], attrs=uattr)

py_wake/site/distance.py
@@ -3,6 +3,7 @@ from numpy import newaxis as na
 import matplotlib
 from py_wake.utils.functions import mean_deg
 from py_wake.utils import gradients
+from py_wake.utils.gradients import rad2deg, deg2rad
 
 
 class StraightDistance():
@@ -132,13 +133,19 @@ class TerrainFollowingDistance(StraightDistance):
         # find height and calculate surface distance
         h = self.site.elevation(x.flatten(), y.flatten()).reshape(x.shape)
         dxy = np.sqrt((x[:, 1] - x[:, 0])**2 + (y[:, 1] - y[:, 0])**2)
+
         dh = np.diff(h, 1, 1)
         s = np.sum(np.sqrt(dxy[:, na]**2 + dh**2), 1)
-
         if upper_tri_only:
-            d_ij = np.zeros(self.dx_ij.shape)
-            d_ij[np.triu(np.eye(len(src_x_i)) == 0)] = s  # set upper triangle
-            d_ij[np.tril(np.eye(len(src_x_i)) == 0)] = s  # set lower triangle
+            # d_ij = np.zeros(self.dx_ij.shape)
+            # d_ij[np.triu(np.eye(len(src_x_i)) == 0)] = s  # set upper triangle
+
+            # same as above without item assignment
+            n = len(src_x_i)
+            d_ij = np.array([np.concatenate([[0] * (i + 1),
+                                             s[int(n * i - (i * (i + 1) / 2)):][:n - i - 1]])
+                             for i in range(n)])  # set upper and lower triangle
+            d_ij += d_ij.T
         else:
             d_ij = s.reshape(self.dx_ij.shape)
         self.d_ij = d_ij
@@ -149,15 +156,15 @@ class TerrainFollowingDistance(StraightDistance):
         # we offset the wind direction by the direction between source and destination
         _, hcw_ijl, dh_ijl = StraightDistance.__call__(self, WD_il, src_idx=src_idx, dst_idx=dst_idx)
         if len(np.shape(WD_il)) == 1:
-            dir_ij = 90 - np.rad2deg(self.theta_ij[src_idx, dst_idx])
+            dir_ij = 90 - rad2deg(self.theta_ij[src_idx, dst_idx])
             wdir_offset_ij = np.asarray(WD_il)[na] - dir_ij
-            theta_ij = np.deg2rad(90 - wdir_offset_ij)
+            theta_ij = deg2rad(90 - wdir_offset_ij)
             sin_ij = np.sin(theta_ij)
             dw_ijl = - sin_ij * self.d_ij[src_idx, dst_idx]
         else:
-            dir_ij = 90 - np.rad2deg(self.theta_ij[src_idx, ][:, dst_idx])
+            dir_ij = 90 - rad2deg(self.theta_ij[src_idx, ][:, dst_idx])
             wdir_offset_ijl = np.asarray(WD_il)[:, na] - dir_ij[:, :, na]
-            theta_ijl = np.deg2rad(90 - wdir_offset_ijl)
+            theta_ijl = deg2rad(90 - wdir_offset_ijl)
             sin_ijl = np.sin(theta_ijl)
             dw_ijl = - sin_ijl * self.d_ij[src_idx][:, dst_idx][:, :, na]
 

py_wake/tests/test_sites/test_distances.py
@@ -31,7 +31,7 @@ class HalfCylinder(UniformSite):
         return np.sqrt(np.maximum(self.height**2 - x_i**2, 0))
 
 
-class Rectangle(TerrainFollowingDistance, UniformSite):
+class Rectangle(UniformSite):
     def __init__(self, height, width, distance_resolution):
         self.height = height
         self.width = width
@@ -201,6 +201,29 @@ def test_distance_over_rectangle():
 
     ref = [9., 3.42, 3.8, 6.02, 6.17, 6.31, 6.43, 7.29, 7.35, 7.41, 7.47, 7.53, 7.58]
     npt.assert_array_almost_equal(Z[my, :25:2], ref, 2)
+
+
+def test_distance_over_rectangle2():
+    site = Rectangle(height=200, width=100, distance_resolution=200)
+
+    x_j = np.arange(-200, 500, 10)
+    y_j = x_j * 0
+    site.distance.setup([-200], [0], [130], (x_j, y_j, y_j + 130))
+    d = site.distance([270])[0][0]
+
+    ref = x_j - x_j[0]
+    ref[x_j > -50] += 200
+    ref[x_j >= 50] += 200
+
+    if 0:
+        H = site.elevation(x_j, y_j)
+        plt.plot(x_j, H, '.-', label='terrain')
+        plt.plot(x_j, d, label='distance')
+        plt.plot(x_j, ref, label='reference')
+        plt.legend()
+        plt.show()
+
+    npt.assert_allclose(d, ref, rtol=0.01)
 
 
 def test_distance_plot():

py_wake/utils/grid_interpolator.py
@@ -144,8 +144,10 @@ class EqDistRegGrid2DInterpolator():
             yi0 = np.minimum(np.maximum(yi0, self.yi_valid_min), self.yi_valid_max - 2)
         xi1 = xi0 + (xif > 0)
         yi1 = yi0 + (yif > 0)
-        valid = (xif >= 0) & (yif >= 0) & (xi1 < len(self.x)) & (yi1 < len(self.y))
-        z = np.empty_like(xp) + np.nan
+        if isinstance(xp, ArrayBox) or isinstance(yp, ArrayBox):
+            valid = slice(None)
+        else:
+            valid = (xif >= 0) & (yif >= 0) & (xi1 < len(self.x)) & (yi1 < len(self.y))
         xi0, xi1, xif, yi0, yi1, yif = [v[valid] for v in [xi0, xi1, xif, yi0, yi1, yif]]
         z00 = self.Z[xi0, yi0]
         z10 = self.Z[xi1, yi0]
@@ -153,5 +155,9 @@ class EqDistRegGrid2DInterpolator():
         z11 = self.Z[xi1, yi1]
         z0 = z00 + (z10 - z00) * xif
         z1 = z01 + (z11 - z01) * xif
-        z[valid] = z0 + (z1 - z0) * yif
+        if isinstance(xp, ArrayBox) or isinstance(yp, ArrayBox):
+            z = z0 + (z1 - z0) * yif
+        else:
+            z = np.empty_like(xp) + np.nan
+            z[valid] = z0 + (z1 - z0) * yif
         return z