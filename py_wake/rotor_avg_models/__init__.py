51b8a66a9cc92b415406eb0fcc7160c6de57a08a
mmpe <mmpe@dtu.dk>
2022-06-16 13:30:07 +0200

    add WSPowerRotorAvg

--- py_wake/rotor_avg_models/__init__.py
+++ py_wake/rotor_avg_models/__init__.py
@@ -1,2 +1,2 @@
 from .rotor_avg_model import RotorAvgModel, EqGridRotorAvg, GQGridRotorAvg, GridRotorAvg, PolarGridRotorAvg, RotorCenter,\
-    CGIRotorAvg, polar_gauss_quadrature, gauss_quadrature
+    CGIRotorAvg, WSPowerRotorAvg, polar_gauss_quadrature, gauss_quadrature
