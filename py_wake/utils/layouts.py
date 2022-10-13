9bcd6a24f206a37bb2fefe0160779b3dd28f32a3
mmpe <mmpe@dtu.dk>
2022-10-13 15:07:59 +0200

    add method to generate circular layouts

--- py_wake/utils/layouts.py
+++ py_wake/utils/layouts.py
@@ -7,3 +7,10 @@ def square(N, distance):
 
 def rectangle(N, columns, distance):
     return [np.ravel(v)[:N] * distance for v in np.meshgrid(np.arange(columns), np.arange(np.ceil(N / columns)))]
+
+
+def circular(wt_in_orbits=[1, 5, 10], outer_radius=1300):
+    assert wt_in_orbits[0] <= 1
+    return np.array([(r * np.cos(theta), r * np.sin(theta))
+                     for n, r in zip(wt_in_orbits, np.linspace(0, outer_radius, len(wt_in_orbits)))
+                     for theta in np.linspace(0, 2 * np.pi, n, endpoint=False)])
