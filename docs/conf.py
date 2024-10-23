a440c4bfcd10e64f1344682d7a8b8b979577f4de
Mads M. Pedersen <mmpe@dtu.dk>
2024-10-23 10:26:58 +0200

    fix docs build and include only specified data files

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -127,7 +127,7 @@ nbsphinx_timeout = 180
 
 # This is processed by Jinja2 and inserted before each notebook
 nbsphinx_prolog = r"""
-{% set docname = 'docs/' + env.doc2path(env.docname, base=None) %}
+{% set docname = 'docs' / env.doc2path(env.docname, base=None) %}
 
 
 .. only:: html
